65c25e406cd4b98cf197cb9d94e9a3b90cabe2a9
blaxsior <heejun2798@gmail.com>
2023-09-22 16:28:01 +0900

    feat: 읽어 온 데이터를 sqs에 저장하는 로직 작성

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,52 @@
+// https://docs.aws.amazon.com/ko_kr/sdk-for-javascript/v2/developer-guide/s3-example-creating-buckets.html
+// https://www.youtube.com/watch?v=SPyDoTkRolY
+import { EventBridgeHandler, S3Event } from 'aws-lambda';
+import { createWriteStream } from 'fs';
+import { SQSClient, SendMessageCommand } from '@aws-sdk/client-sqs';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 import { getNewsAndCommentResults } from './crawling/index.js';
+import { genRandomFileName } from './util/gen-key.js';
+
+
+const sqsURL = process.env.SQS_URL;
+const region = process.env.REGION;
+const bucket = process.env.BUCKET_NAME;
+
+const sqs = new SQSClient({ region });
+const s3 = new S3Client({
+  region,
+});
+
 export const handler = async (): Promise<void> => {
-  const result = await getNewsAndCommentResults('윤석열', ['1002']);
-  console.log(result);
+  const keyword = '윤석열';
+  const list = ['1002'];
+  // 키워드를 읽어와서 아래와 같이 처리해야 함...
+  const fileName = genRandomFileName();
+  const result = await getNewsAndCommentResults(keyword, list);
+ 
+  const sqsCommand = new SendMessageCommand({
+    QueueUrl: sqsURL,
+    MessageBody: JSON.stringify({
+      keyword: keyword,
+      key: fileName,
+    }),
+  });
+
+  const s3Command = new PutObjectCommand({
+    Bucket: bucket,
+    Key: fileName,
+    Body: JSON.stringify(result),
+  });
+
+  try {
+    const s3result = await s3.send(s3Command);
+    const sqsresult = await sqs.send(sqsCommand);
+  } catch(e) {
+    console.log(e);
+  }
 }
+
+
+// (async function () {
+//   await handler();
+// })();
